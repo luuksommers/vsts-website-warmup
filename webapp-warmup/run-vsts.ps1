b3627cfb6184e7346ab1a0702cf38c5c5760af12
Luuk Sommers <luuk.sommers@infocaster.net>
2018-02-08 11:06:38 +0100

    Update parameter types

--- webapp-warmup/run-vsts.ps1
+++ webapp-warmup/run-vsts.ps1
@@ -1,9 +1,9 @@
 Write-Verbose "Entering script run-vsts.ps1"
 
 $rootUrl = Get-VstsInput -Name Url
-$retryCount = Get-VstsInput -Name RetryCount
-$sleepPeriod = Get-VstsInput -Name SleepPeriod
-$ignoreError = Get-VstsInput -Name IgnoreError
+$retryCount = Get-VstsInput -Name RetryCount -AsInt
+$sleepPeriod = Get-VstsInput -Name SleepPeriod -AsInt
+$ignoreError = Get-VstsInput -Name IgnoreError -AsBool
 $suffixes = Get-VstsInput -Name Suffixes
 
 .\webapp-warmup.ps1  -rootUrl $rootUrl -retryCount $retryCount -sleepPeriod $sleepPeriod -ignoreError $ignoreError -suffixes $suffixes
